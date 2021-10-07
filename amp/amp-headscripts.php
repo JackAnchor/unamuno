07ed0feb7afc4a9bb55f5e7c133fd40958a4cc30
JackAnchor <Jack@threeanchorgroup.com>
2021-10-07 15:05:34 -0500

    Add files via upload
    AMP-related header scripts and AMP-valid metadata.

---
+++ amp/amp-headscripts.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Loads all AMP components and custom AMP functions into <head>
+ *
+ * Loads all site-wide AMP JS components into <head>.
+ * Loads any custom AMP JS functions.
+ * Immediately follows AMP boilerplate code and precedes CSS.
+ */
+?>
+  <script async custom-element="amp-analytics" src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"></script> <!-- analytics --> <script async custom-element="amp-fit-text" src="https://cdn.ampproject.org/v0/amp-fit-text-0.1.js"></script> <!-- fit tx --> <script async custom-element="amp-fx-collection" src="https://cdn.ampproject.org/v0/amp-fx-collection-0.1.js"></script> <!-- parallax title --> <script async custom-element="amp-fx-flying-carpet" src="https://cdn.ampproject.org/v0/amp-fx-flying-carpet-0.1.js"></script> <!-- carpet -->
+  <script async custom-element="amp-sidebar" src="https://cdn.ampproject.org/v0/amp-sidebar-0.1.js"></script> <!-- sidebar --> <script async custom-element="amp-accordion" src="https://cdn.ampproject.org/v0/amp-accordion-0.1.js"></script> <!-- accordion drop --> <script async custom-element="amp-carousel" src="https://cdn.ampproject.org/v0/amp-carousel-0.1.js"></script>
+<!-- carousel --> <script async custom-element="amp-image-lightbox" src="https://cdn.ampproject.org/v0/amp-image-lightbox-0.1.js"></script> <!-- img lightbox --> <script async custom-element="amp-lightbox" src="https://cdn.ampproject.org/v0/amp-lightbox-0.1.js"></script> <!-- lightbox --> <script async custom-template="amp-mustache" src="https://cdn.ampproject.org/v0/amp-mustache-0.2.js"></script> <!-- mustache -->
+<script async custom-element="amp-form" src="https://cdn.ampproject.org/v0/amp-form-0.1.js"></script> <!-- form -->
+<script async custom-element="amp-youtube" src="https://cdn.ampproject.org/v0/amp-youtube-0.1.js"></script>
+  <?php unamuno_add_animation_script(); ?>
